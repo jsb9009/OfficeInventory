adb96415c94a0ec9a8982167eb545b71f5a413da
Udara Seneviratne <udara.seneviratne@hsenidmobile.com>
2017-07-11 17:07:32 +0530

    included comments on naming conventions in mysql

--- inventory_dump.sql
+++ inventory_dump.sql
@@ -15,6 +15,17 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+
+/*Databases are named like eg: hms_service_hub
+Tables are named as eg: system_permission
+Table fields named as eg: register_date
+ */
+
+
+
+
+
 --
 -- Table structure for table `employee`
 --
